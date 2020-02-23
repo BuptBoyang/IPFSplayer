c16b8d1c7c9ce03b9cb58949abb8cff7106fb6b6
Boyang Zhang <zby.che9@gmail.com>
2020-02-23 20:06:37 +0800

    2.23

--- app/src/main/java/indi/boyang/ipfsplayer/api/MyService.kt
+++ app/src/main/java/indi/boyang/ipfsplayer/api/MyService.kt
@@ -4,9 +4,14 @@ import androidx.lifecycle.LiveData
 import indi.boyang.ipfsplayer.models.Video
 import indi.boyang.ipfsplayer.util.LiveDataCallAdapterFactory
 import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
+import java.util.concurrent.TimeUnit
+
 
 interface MyService {
     @GET("/videos")
@@ -17,22 +22,39 @@ interface MyService {
         @Path("id") id: Long
     ): LiveData<Video>
 
-    @FormUrlEncoded
+    @Headers("Content-Type:multipart/form-data")
+    @Multipart
     @POST("/videos")
     fun uploadVideo(
-        @Field("title") title: String,
-        @Field("pic") picFile: MultipartBody.Part,
-        @Field("video") videoFile: MultipartBody.Part
-    ):LiveData<Video>
+        @Part("title") title: String,
+        @Part pic: MultipartBody.Part,
+        @Part video: MultipartBody.Part
+    ): Call<ResponseBody>
+
+    @FormUrlEncoded
+    @POST("/test")
+    fun postTest(
+        @Field("num") n:Int
+    ):Call<ResponseBody>
+
 
     companion object Factory {
         fun create(): MyService {
             return Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(LiveDataCallAdapterFactory())
-                .baseUrl("http://35.203.163.14:8081") //https://api.noroo.xyz, http://10.0.2.2:8081
+                .baseUrl("http://10.0.2.2:8081") //https://api.noroo.xyz, http://35.203.163.14:8081
+                .build()
+                .create(MyService::class.java)
+        }
+
+        fun create2(): MyService {
+            return Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("http://10.0.2.2:8081") //https://api.noroo.xyz, http://35.203.163.14:8081
                 .build()
                 .create(MyService::class.java)
         }
     }
 }
+
--- app/src/main/java/indi/boyang/ipfsplayer/ui/send/SendFragment.kt
+++ app/src/main/java/indi/boyang/ipfsplayer/ui/send/SendFragment.kt
@@ -1,6 +1,8 @@
 package indi.boyang.ipfsplayer.ui.send
 
+import android.content.ContentResolver
 import android.content.Intent
+import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -13,6 +15,9 @@ import kotlinx.android.synthetic.main.fragment_send.*
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Response
 import java.io.File
 
 
@@ -22,6 +27,8 @@ class SendFragment : Fragment() {
     private val VIDEO_REQUEST_CODE = 2
     private var coverUri: Uri? = null
     private var videoUri: Uri? = null
+    private lateinit var coverPath:String
+    private lateinit var videoPath:String
 
     private lateinit var sendViewModel: SendViewModel
 
@@ -47,19 +54,30 @@ class SendFragment : Fragment() {
 
         buttonChooseVideo.setOnClickListener {
             val pickVideoIntent = Intent(Intent.ACTION_PICK)
-            pickVideoIntent.type = "video/*"
+            pickVideoIntent.type = "video/mp4"
             startActivityForResult(pickVideoIntent,VIDEO_REQUEST_CODE)
         }
 
         buttonUpload.setOnClickListener{
+
             if(coverUri!=null && videoUri!=null){
-                MyService.create().uploadVideo(
+                MyService.create2().uploadVideo(
                     editTitle.text.toString(),
-                    uriToMultipart(coverUri!!),
-                    uriToMultipart(videoUri!!)
-                )
-                uploadResultView.text = "finish"
-                //println(editTitle.text.toString())
+                    pathToMultipart(coverPath,"pic"),
+                    pathToMultipart(videoPath,"video")
+                ).enqueue(object : retrofit2.Callback<ResponseBody> {
+                    override fun onResponse(
+                        call: Call<ResponseBody>,
+                        response: Response<ResponseBody>
+                    ) {
+                        uploadResultView.text = "finish"
+                    }
+                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                        uploadResultView.text = t.message
+                    }
+                })
+            } else {
+                uploadResultView.text = "Please select cover and video file"
             }
         }
     }
@@ -68,21 +86,40 @@ class SendFragment : Fragment() {
         when(requestCode){
             PHOTO_REQUEST_CODE -> if (data != null) {
                 coverUri = data.data
-                coverFileName.text = coverUri.toString()
+                val resolver: ContentResolver = this.context!!.contentResolver
+                val cursor: Cursor = resolver.query(coverUri, null, null, null, null) ?: return
+                if (cursor.moveToFirst()) {
+                    coverPath = cursor.getString(cursor.getColumnIndex("_data"))
+                }
+                cursor.close()
+                coverFileName.text = coverPath
             }
             VIDEO_REQUEST_CODE -> if (data != null) {
                 videoUri = data.data
-                videoFileName.text = videoUri.toString()
+                val resolver: ContentResolver = this.context!!.contentResolver
+                val cursor: Cursor = resolver.query(videoUri, null, null, null, null) ?: return
+                if (cursor.moveToFirst()) {
+                    videoPath = cursor.getString(cursor.getColumnIndex("_data"))
+                }
+                cursor.close()
+                videoFileName.text = videoPath
             }
         }
     }
 
-    private fun uriToMultipart(uri: Uri): MultipartBody.Part {
+    private fun pathToMultipart(path:String,name:String): MultipartBody.Part {
+        val file = File(path)
+        val requestFile =
+            RequestBody.create(MediaType.parse("multipart/form-data"), file)
+        return MultipartBody.Part.createFormData(name, file.name, requestFile)
+    }
+
+    private fun uriToMultipart(uri: Uri,name:String): MultipartBody.Part {
         val file = File(uri.path)
         val requestFile =
             RequestBody.create(MediaType.parse("multipart/form-data"), file)
-        return MultipartBody.Part.createFormData("file", file.name, requestFile)
+        return MultipartBody.Part.createFormData(name, file.name, requestFile)
     }
 
-
 }
+
